accae744b532e6347887147f682aed1d4e47078d
Raphael Michel <mail@raphaelmichel.de>
2021-01-08 22:56:26 +0100

    Direct debit: Default to pain.008.001.02

--- tests/debit/test_00800302.py
+++ tests/debit/test_00800302.py
@@ -3,6 +3,7 @@ import datetime
 import pytest
 
 from sepaxml import SepaDD
+from sepaxml.validation import ValidationError
 from tests.utils import clean_ids, validate_xml
 
 
@@ -213,3 +214,55 @@ def test_two_debits(sdd):
     xmlout = sdd.export()
     xmlpretty = validate_xml(xmlout, "pain.008.003.02")
     assert clean_ids(xmlpretty.strip()) == clean_ids(SAMPLE_RESULT.strip())
+
+
+def test_name_too_long():
+    sdd = SepaDD({
+        "name": "TestCreditor",
+        "BIC": "BANKNL2A",
+        "IBAN": "NL50BANK1234567890",
+        "batch": True,
+        "creditor_id": "000000",
+        "currency": "EUR"
+    }, schema="pain.008.003.02")
+    payment1 = {
+        "name": "Test von Testenstein Test von Testenstein Test von Testenstein Test von Testenstein",
+        "IBAN": "NL50BANK1234567890",
+        "BIC": "BANKNL2A",
+        "amount": 1012,
+        "type": "FRST",
+        "collection_date": datetime.date.today(),
+        "mandate_id": "1234",
+        "mandate_date": datetime.date.today(),
+        "description": "Test transaction1"
+    }
+    sdd.add_payment(payment1)
+    with pytest.raises(ValidationError):
+        sdd.export()
+    sdd.export(validate=False)
+
+
+def test_invalid_mandate():
+    sdd = SepaDD({
+        "name": "TestCreditor",
+        "BIC": "BANKNL2A",
+        "IBAN": "NL50BANK1234567890",
+        "batch": True,
+        "creditor_id": "000000",
+        "currency": "EUR"
+    }, schema="pain.008.003.02")
+    payment1 = {
+        "name": "Test von Testenstein",
+        "IBAN": "NL50BANK1234567890",
+        "BIC": "BANKNL2A",
+        "amount": 1012,
+        "type": "FRST",
+        "collection_date": datetime.date.today(),
+        "mandate_id": "1234ÄOÜ",
+        "mandate_date": datetime.date.today(),
+        "description": "Test transaction1"
+    }
+    sdd.add_payment(payment1)
+    with pytest.raises(ValidationError):
+        sdd.export()
+    sdd.export(validate=False)
